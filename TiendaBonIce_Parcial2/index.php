4ca52b7dcdeb980d7726cf43a83bbdc837edb36e
Alexander <114307680+Alexander37ss@users.noreply.github.com>
2022-11-03 19:32:02 -0700

    Menu e Index

---
+++ TiendaBonIce_Parcial2/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+    <title>BonIce</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<style>
+    .fondo{
+        background-color: #17a2b8;
+    }
+</style>
+<body class="fondo">
+    <?php include 'menu.php'; ?>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                    <h1 class="display-4">Encuentra tus BonIce favoritos</h1>
+                    <p class="lead">
+                        El rincon donde podras comprar BonIce:
+                         <ul>
+                            <li>Ver catalogo</li>
+                            <li>Registrar nuevos productos</li>
+                            <li>Editar productos</li>
+                            <li>Eliminar productos</li>
+                         </ul>
+                    </p>
+                    <hr class="my-4">
+                    <p>PROYECTO PARCIAL 2</p>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center">
+        <hr>
+        2022 &copy; Cetis107 BonIce
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ TiendaBonIce_Parcial2/menu.php
@@ -0,0 +1,17 @@
+<div class="fixed-top">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h5 class="text-white h4"><a class="navbar-brand" href="index.php"><img src="img/pinguino.png" alt="" width="75px"></a></h5>
+            <span class="text-muted">
+                    <a class="text-white h4" href="index.php">Inicio</a><br>
+                    <a class="text-white h4" href="">Registrar</a><br>
+                    <a class="text-white h4" href="">Consultar</a>
+            </span>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
