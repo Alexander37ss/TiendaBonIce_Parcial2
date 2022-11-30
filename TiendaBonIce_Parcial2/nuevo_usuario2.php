818f182087ed339cea8cf9c5b118fc5d9af097ef
AryDaniel <ary.lopez@cetis107.edu.mx>
2022-11-29 20:14:19 -0700

    Capacidad de no repetir correo

--- TiendaBonIce_Parcial2/nuevo_usuario2.php
+++ TiendaBonIce_Parcial2/nuevo_usuario2.php
@@ -1,19 +1,40 @@
 <?php
 	session_start();
     
-    NuevoUsuario($_POST['correo'],$_POST['nombre'],$_POST['contrasena']);
+    $correo = $_POST['correo'];
+    $usuario = $_POST['nombre'];
+    $contrasena = $_POST['contrasena'];
 
-    function NuevoUsuario($correo, $usuario, $contrasena){
+    if(buscaRepetido($correo)==1){
+        echo '<script>';
+            echo 'alert("Usuario repetido");';
+            echo 'window.location.href="index.php";';
+        echo '</script>';
+    }else{
         include 'conexionConBD.php';
 
         $sentencia="INSERT INTO usuarios (correo, usuario, password) 
         VALUES('".$correo."','".$usuario."','".$contrasena."') ";
         
-			$estado='activo';
-			$_SESSION['estado']=$estado;
+            $estado='activo';
+            $_SESSION['estado']=$estado;
             
         $conexion->query($sentencia);
         echo '<script>';
             echo 'window.location.href="entro/index.php";';
         echo '</script>';
+    }
+
+    function buscaRepetido($email){
+        include 'conexionConBD.php';
+
+        $sql="SELECT * FROM usuarios 
+            WHERE correo='$email'";
+
+        $contar = $conexion->query($sql);
+        if(mysqli_num_rows($contar) > 0){
+            return 1;
+        }else{
+            return 0;
+        }
     }
